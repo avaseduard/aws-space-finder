bf5de4e92cba56f0587078b7ef13f4bba253e4de
eduard <avas.eduard@gmail.com>
2024-02-05 07:38:29 +0200

    use sdk to list all buckets

--- src/services/hello.ts
+++ src/services/hello.ts
@@ -1,15 +1,22 @@
+import { ListBucketsCommand, S3Client } from "@aws-sdk/client-s3";
 import {
   APIGatewayProxyEvent,
   APIGatewayProxyResult,
   Context,
 } from "aws-lambda";
-import { v4 } from "uuid";
+
+const s3Client = new S3Client({});
 
 async function handler(event: APIGatewayProxyEvent, context: Context) {
+  const command = new ListBucketsCommand({});
+  const listBucketsResult = (await s3Client.send(command)).Buckets;
+
   const response: APIGatewayProxyResult = {
     statusCode: 200,
     body: JSON.stringify(
-      `Hello, I'm Lambda and this is a random UUID: ${v4()}.`
+      `Hello, I'm Lambda and this is the bucket list ${JSON.stringify(
+        listBucketsResult
+      )}.`
     ),
   };
   console.log(event);
