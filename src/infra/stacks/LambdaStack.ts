6e1d5f9f74fcc5798c78689029eb43f24163719a
eduard <avas.eduard@gmail.com>
2024-02-06 19:55:13 +0200

    created a post request to dynamodb

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -1,10 +1,11 @@
-import { Stack, StackProps } from "aws-cdk-lib";
-import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
-import { ITable } from "aws-cdk-lib/aws-dynamodb";
-import { Runtime } from "aws-cdk-lib/aws-lambda";
-import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
-import { Construct } from "constructs";
-import { join } from "path";
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
+import { ITable } from 'aws-cdk-lib/aws-dynamodb';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Construct } from 'constructs';
+import { join } from 'path';
 
 interface LambdaStackProps extends StackProps {
   spacesTable: ITable;
@@ -16,14 +17,23 @@ export class LambdaStack extends Stack {
   constructor(scope: Construct, id: string, props: LambdaStackProps) {
     super(scope, id, props);
 
-    const spacesLambda = new NodejsFunction(this, "SpacesLambda", {
+    const spacesLambda = new NodejsFunction(this, 'SpacesLambda', {
       runtime: Runtime.NODEJS_20_X,
-      handler: "handler",
-      entry: join(__dirname, "..", "..", "services", "spaces", "handler.ts"),
+      handler: 'handler',
+      entry: join(__dirname, '..', '..', 'services', 'spaces', 'handler.ts'),
       environment: {
         TABLE_NAME: props.spacesTable.tableName,
       },
     });
+
+    // Allow Lambda to manage DynamoDb
+    spacesLambda.addToRolePolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        resources: [props.spacesTable.tableArn],
+        actions: ['dynamodb:PutItem'],
+      })
+    );
 
     this.spacesLambdaIntegration = new LambdaIntegration(spacesLambda);
   }
--- src/services/spaces/handler.ts
+++ src/services/spaces/handler.ts
@@ -1,8 +1,13 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import {
   APIGatewayProxyEvent,
   APIGatewayProxyResult,
   Context,
-} from "aws-lambda";
+} from 'aws-lambda';
+import { postSpaces } from './PostSpaces';
+
+// Initialize DynamoDb
+const ddbClient = new DynamoDBClient({});
 
 async function handler(
   event: APIGatewayProxyEvent,
@@ -10,16 +15,23 @@ async function handler(
 ): Promise<APIGatewayProxyResult> {
   let message: string;
 
-  switch (event.httpMethod) {
-    case "GET":
-      message = "Hello from GET";
-      break;
-    case "POST":
-      message = "Hello from POST";
-      break;
-
-    default:
-      break;
+  try {
+    switch (event.httpMethod) {
+      case 'GET':
+        message = 'Hello from GET';
+        break;
+      case 'POST':
+        const response = postSpaces(event, ddbClient);
+        return response;
+      default:
+        break;
+    }
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error.message),
+    };
   }
 
   const response: APIGatewayProxyResult = {
